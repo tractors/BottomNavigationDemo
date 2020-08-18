20df82114616296c55fcaa3127f40824e596d8a8
tractors <huijianhua@163.com>
2020-08-18 15:28:44 +0800

    add bottomNavigationView and objectAnimatior

---
+++ app/src/main/java/com/will/bottomnavigationdemo/FirstFragment.java
@@ -0,0 +1,52 @@
+package com.will.bottomnavigationdemo;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class FirstFragment extends Fragment {
+
+    private FirstViewModel mViewModel;
+    private ImageView imageView;
+
+    public static FirstFragment newInstance() {
+        return new FirstFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.first_fragment, container, false);
+        imageView = view.findViewById(R.id.imageView);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(FirstViewModel.class);
+        final ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(imageView,"rotation",0,0);
+        objectAnimator.setDuration(500);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!objectAnimator.isRunning()){
+                    objectAnimator.setFloatValues(imageView.getRotation(),imageView.getRotation() + 100);
+                    objectAnimator.start();
+                }
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/will/bottomnavigationdemo/MainActivity.java
+++ app/src/main/java/com/will/bottomnavigationdemo/MainActivity.java
@@ -1,8 +1,15 @@
 package com.will.bottomnavigationdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        NavController navController = Navigation.findNavController(this,R.id.fragment);
+
+        AppBarConfiguration configuration = new AppBarConfiguration.Builder(bottomNavigationView.getMenu()).build();
+
+        NavigationUI.setupActionBarWithNavController(this,navController,configuration);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
     }
 }
